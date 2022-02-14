e2b9c66b57b43ef312c60668fa2fa38edf7c55f8
ericbertrand8686 <ericbertrand8686@yahoo.fr>
2022-02-14 16:50:33 +0100

    feat : MVP wordcounter

--- Flask_API/get_bs_parse.py
+++ Flask_API/get_bs_parse.py
@@ -1,9 +1,21 @@
 from bs4 import BeautifulSoup
 from requests import get
+import pandas as pd
 
 url = 'https://fr.wikipedia.org/wiki/Representational_state_transfer'
 
 response = get(url)
 html_soup = BeautifulSoup(response.text, 'html.parser')
 content = html_soup.main.get_text()
+
+content.split().value_counts()
+
+c_split = content.split()
+
+w_count= {}
+for wd in c_split:
+  if wd in w_count :
+    w_count[wd]+= 1
+  else:
+    w_count[wd]= 1
 
