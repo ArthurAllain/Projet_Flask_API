210f8a2555f5f33be3bdbfa92596a5ca82889009
ericbertrand8686 <ericbertrand8686@yahoo.fr>
2022-02-15 13:52:20 +0100

    updt : ch name of test 2 main

--- Flask_API/test_Pyson.py
+++ Flask_API/test_Pyson.py
@@ -2,6 +2,9 @@ from pysondb import db
 
 apidb=db.getDb('flask_api_db.json')
 
-apidb.add({"name":"pysondb","type":"DB"})
-apidb.add({"namme":"pyson","type":"DB"})
+# apidb.add({"name":"pysondb","type":"DB"})
+
 # The data wont be added as the key "name" is mispelled as "namme"
+# apidb.add({"namme":"pyson","type":"DB"})
+
+print(a.get())
