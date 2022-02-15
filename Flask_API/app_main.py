b75a918681a9207c23c81688b7bf25abcc494e48
ericbertrand8686 <ericbertrand8686@yahoo.fr>
2022-02-15 14:16:57 +0100

    feat : route placeholder

--- Flask_API/app_main.py
+++ Flask_API/app_main.py
@@ -7,7 +7,31 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return ("Hello World!")
+    return ("app en fonction")
+
+@app.route('/create')
+def create():
+    return
+
+@app.route('/read')
+def read():
+    return
+
+@app.route('/update')
+def update():
+    return
+
+@app.route('/delete')
+def delete():
+    return
+
+@app.route('/graph')
+def graph():
+    return
+
+@app.route('/flush')
+def flush():
+    return
 
 #run the application
 if __name__ == "__main__":
--- Flask_API/test_Pyson.py
+++ Flask_API/test_Pyson.py
@@ -2,9 +2,11 @@ from pysondb import db
 
 apidb=db.getDb('flask_api_db.json')
 
-# apidb.add({"name":"pysondb","type":"DB"})
+#apidb.add({"name":"py_cli","type":"CLI"})
 
 # The data wont be added as the key "name" is mispelled as "namme"
 # apidb.add({"namme":"pyson","type":"DB"})
 
-print(a.get())
+#print(apidb.get(1))
+
+print(apidb.getBy({"type":"CLI"}))
