07702a6b98701eda0411dc41e07b8f04e752204d
ericbertrand8686 <ericbertrand8686@yahoo.fr>
2022-02-16 14:34:13 +0100

    feat : parsing fonctionnel ds create

--- Flask_API/app_main.py
+++ Flask_API/app_main.py
@@ -3,6 +3,12 @@ from flask import Flask
 from flask import request
 from pysondb import db
 
+import nltk
+from newspaper import Article
+nltk.download('punkt')
+
+from nltk.tokenize import word_tokenize
+
 # creation de l'application
 app = Flask(__name__)
 
@@ -13,17 +19,24 @@ apidb=db.getDb('flask_api_db.json')
 def home():
     return ("API active")
 
-@app.route('/create/<url>')
-def create(url):
-    #url = request.args.get('url')
-    # creation liste de tuples mots : décompte (placeholder actuellement)
-    vocabulary= {'crise': 0, 'des': 8, 'du': 4, 'en': 9, 'et': 6,  'la': 11, 'le': 5, 'les': 8, 'pour': 5,  'pourquoi': 1, 'se': 3, 'ukraine': 0, 'à': 8}
-    newid = apidb.add({"name": url,"type":"entry", "words":vocabulary})
+@app.route('/create')
+def create():
+    # creation liste de tuples mots : décompte
+    #vocabulary= {'crise': 0, 'des': 8, 'du': 4, 'en': 9, 'et': 6,  'la': 11, 'le': 5, 'les': 8, 'pour': 5,  'pourquoi': 1, 'se': 3, 'ukraine': 0, 'à': 8}
+    url = request.args.get('url')
+    article = Article(url)
+    article.download()
+    article.parse()
+    article.nlp()
+
+    wrd = word_tokenize(article.text)
+    wrd_count = nltk.FreqDist(wrd)
+    
+    newid = apidb.add({"name": url,"type":"url_entry", "words": wrd_count })
     return str(newid)
 
 @app.route('/read/<currentid>', methods=['GET'])
 def read(currentid):
-    #currentid = request.args.get('id')
     res = apidb.getById(currentid)
     return str(res['words'])
 
